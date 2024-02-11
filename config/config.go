bb11f154df5a93986b6bcb491306ba8a8528daec
Onichaa <91194692+Onichaa@users.noreply.github.com>
2024-02-11 23:18:59 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -7,7 +7,7 @@ var (
 	Name  = "WaSocket Bot"
 	Login = "code"
 	Bot   = "6283873541589"
-	Owner = []string{"628388024064", "6283169480682"}
+	Owner = []string{"628388024064"}
 	Self  = false
 	LolSite = "https://api.lolhuman.xyz/"
 	LolKey = "5f38494f3555283d0446abdf"
